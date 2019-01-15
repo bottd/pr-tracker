345d4c6c5c3a361dc65fcde91d0e8362289b44ce
Drake Bott <bott.da@gmail.com>
2019-01-15 00:19:25 -0700

    Add test directory

---
+++ test/utils.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const utils = require('../utils/utils');
+
+const expect = chai.expect;
+
+describe('utils', () => {
+  describe('parseLinks', () => {
+    it('Should should return an object with <links> assigned to their rel', () => {
+      const expected = {
+        test: 'https://api.github.com',
+      };
+      const links = '<https://api.github.com>; rel="test"';
+      const result = utils.parseLinks(links);
+      expect(result).to.deep.equal(expected);
+    });
+    it('Should work with multiple links', () => {
+      const expected = {
+        next:
+          'https://api.github.com/repositories/10270250/pulls?state=open&page=2',
+        last:
+          'https://api.github.com/repositories/10270250/pulls?state=open&page=5',
+      };
+      const links =
+        '<https://api.github.com/repositories/10270250/pulls?state=open&page=2>; rel="next", <https://api.github.com/repositories/10270250/pulls?state=open&page=5>; rel="last"';
+      const result = utils.parseLinks(links);
+      expect(result).to.deep.equal(expected);
+    });
+  });
+});
