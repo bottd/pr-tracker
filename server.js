144dd5da58cf3847cb176c9427ef004af5ec3088
Drake Bott <bott.da@gmail.com>
2019-01-14 23:32:38 -0700

    configure dotenv

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
+const API = require('./utils/API');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/api/:user/:repo', (req, res) => {
-  const { user, repo } = req.params;
+app.get('/api/:owner/:repo', async (req, res) => {
+  const { owner, repo } = req.params;
+  const data = await API.fetchAllPulls('facebook', 'react');
   // get all open PRs for repo
   // request commits for each repo to get commit counts
   // send in response
-  res.status(200).json({ user, repo });
+  res.status(200).json({ owner, repo });
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
