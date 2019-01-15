701142d82027de2da4deda85bd6239cf575c3a54
Drake Bott <bott.da@gmail.com>
2019-01-15 13:47:47 -0700

    Create API mock module

---
+++ utils/__mocks__/API.js
@@ -0,0 +1,36 @@
+const mockLinks = '<https://example.com/repo?page=2>; rel="next"';
+const mockPulls = [
+  {
+    user: {
+      login: 'bottd',
+    },
+    commits_url: 'example.com/commits/1',
+    comments_url: 'example.com/comments/1',
+    title: 'A Pull Request',
+    extraKey: 'Should be thrown out',
+  },
+  {
+    user: {
+      login: 'bottd',
+    },
+    commits_url: 'example.com/commits/2',
+    comments_url: 'example.com/comments/2',
+    title: 'Another Pull Request',
+    extraKey: 'Should be thrown out',
+  },
+];
+
+const getPulls = jest
+  .fn()
+  .mockImplementationOnce(() => ({
+    links: mockLinks,
+    pulls: mockPulls,
+  }))
+  .mockImplementationOnce(() => ({
+    links: null,
+    pulls: mockPulls,
+  }));
+
+const getData = jest.fn().mockImplementation(() => mockPulls);
+
+module.exports = { getData, getPulls };
