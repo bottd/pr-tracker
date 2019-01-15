916bf9e86c8b4fa89a3503eb3e4535c2bbb04e1b
Drake Bott <bott.da@gmail.com>
2019-01-15 13:24:48 -0700

    Update route to get and clean up data

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const API = require('./utils/API');
+const utils = require('./utils/utils');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/api/:owner/:repo', async (req, res) => {
   const { owner, repo } = req.params;
-  const data = await API.fetchAllPulls('facebook', 'react');
-  // get all open PRs for repo
-  // request commits for each repo to get commit counts
-  // send in response
-  res.status(200).json({ owner, repo });
+  const data = await utils.fetchAllPulls(owner, repo);
+  const cleaned = await utils.cleanPulls(data);
+  res.status(200).json({ owner, repo, data: cleaned });
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
