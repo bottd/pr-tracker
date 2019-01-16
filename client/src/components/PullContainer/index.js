5bbfd79f93c6baf69c9c18d30e1987c9624b6853
Drake Bott <bott.da@gmail.com>
2019-01-16 14:12:15 -0700

    Add message to display when there are no PRs

--- client/src/components/PullContainer/index.js
+++ client/src/components/PullContainer/index.js
@@ -6,5 +6,15 @@ export default function PullsContainer(props) {
   const pulls = props.pulls.map(pull => (
     <PullRequest {...pull} key={pull.id} />
   ));
+  if (!pulls.length) {
+    return (
+      <div className="PullContainer">
+        <h2>
+          There's nothing here! The selected repository may not exist, or have
+          no open pull requests.
+        </h2>
+      </div>
+    );
+  }
   return <div className="PullContainer">{pulls}</div>;
 }
