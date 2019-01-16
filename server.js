234bdd1f179d4bb31080a2c83c147e5aa7852106
Drake Bott <bott.da@gmail.com>
2019-01-16 12:20:25 -0700

    Add try/catch to GET route

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const port = process.env.PORT || 4000;
 
 app.get('/api/:owner/:repo', async (req, res) => {
   const { owner, repo } = req.params;
-  const data = await utils.fetchAllPulls(owner, repo);
-  const cleaned = await utils.cleanPulls(data);
-  res.status(200).json(cleaned);
+  try {
+    const data = await utils.fetchAllPulls(owner, repo);
+    const cleaned = await utils.cleanPulls(data);
+    res.status(200).json(cleaned);
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
