7155258746e4f5078cfcf1648bea9d58bdc4a0da
Drake Bott <bott.da@gmail.com>
2019-01-15 22:37:39 -0700

    Mock cleanData and fetchAllPulls methods for mock module

--- utils/__mocks__/index.js
+++ utils/__mocks__/index.js
@@ -1,5 +1,26 @@
-const cleanPulls = jest.fn()
-const fetchALlPulls = jest.fn()
-const parseLinks = jest.fn()
+const mockPulls = [
+  {
+    user: {
+      login: 'bottd',
+    },
+    commits_url: 'example.com/commits/1',
+    comments_url: 'example.com/comments/1',
+    title: 'A Pull Request',
+    extraKey: 'Should be thrown out',
+  },
+  {
+    user: {
+      login: 'bottd',
+    },
+    commits_url: 'example.com/commits/2',
+    comments_url: 'example.com/comments/2',
+    title: 'Another Pull Request',
+    extraKey: 'Should be thrown out',
+  },
+];
+
+const cleanPulls = jest.fn(() => ['pull1', 'pull2']);
+const fetchAllPulls = jest.fn(() => mockPulls);
+const parseLinks = jest.fn();
 
 module.exports = { cleanPulls, fetchAllPulls, parseLinks };
